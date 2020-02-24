09b09c6b52245cda46a863aa0fa3b7135eed8b4e
soruh <mail@soruh.de>
2020-02-24 21:56:37 +0100

    fixed tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -36,7 +36,7 @@ fn open_file() {
     let lib = Library::new();
 
     let file = lib
-        .open("testcgns", CgnsOpenMode::Write)
+        .open("test.cgns", CgnsOpenMode::Write)
         .expect("Failed to open file");
 
     file.close().expect("Failed to close file");
@@ -47,7 +47,7 @@ fn get_cgio() {
     let lib = Library::new();
 
     let file = lib
-        .open("testcgns", CgnsOpenMode::Write)
+        .open("test.cgns", CgnsOpenMode::Write)
         .expect("Failed to open file");
 
     let cgio = file.cgio().expect("Failed to get cgio");
@@ -60,10 +60,10 @@ fn get_cgio() {
 fn test_goto() {
     let lib = Library::new();
 
-    create_file(&lib, "goto_testcgns").expect("Failed to create file");
+    create_file(&lib, "goto_test.cgns").expect("Failed to create file");
 
     let mut file = lib
-        .open("goto_testcgns", CgnsOpenMode::Modify)
+        .open("goto_test.cgns", CgnsOpenMode::Modify)
         .expect("failed to open file");
 
     let base_index = Base::write(
@@ -90,10 +90,10 @@ fn test_goto() {
 fn iter_bases() {
     let lib = Library::new();
 
-    create_file(&lib, "iter_testcgns").expect("Failed to create file");
+    create_file(&lib, "iter_test.cgns").expect("Failed to create file");
 
     let mut file = lib
-        .open("iter_testcgns", CgnsOpenMode::Modify)
+        .open("iter_test.cgns", CgnsOpenMode::Modify)
         .expect("failed to open file");
 
     assert_eq!(
@@ -146,10 +146,10 @@ fn iter_bases() {
 fn read_write_base_and_descriptor() {
     let lib = Library::new();
 
-    create_file(&lib, "base_testcgns").expect("Failed to create file");
+    create_file(&lib, "base_test.cgns").expect("Failed to create file");
 
     let mut file = lib
-        .open("base_testcgns", CgnsOpenMode::Modify)
+        .open("base_test.cgns", CgnsOpenMode::Modify)
         .expect("Failed to open file");
 
     let base_data = base::BaseData {
@@ -188,10 +188,10 @@ fn read_write_base_and_descriptor() {
 fn read_write_zone_and_descriptor() {
     let lib = Library::new();
 
-    create_file(&lib, "desc_testcgns").expect("Failed to create file");
+    create_file(&lib, "desc_test.cgns").expect("Failed to create file");
 
     let mut file = lib
-        .open("desc_testcgns", CgnsOpenMode::Modify)
+        .open("desc_test.cgns", CgnsOpenMode::Modify)
         .expect("Failed to open file");
 
     let base_data = base::BaseData {
